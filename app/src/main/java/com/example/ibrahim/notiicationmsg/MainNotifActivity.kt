42a672cd97e6496450838d53be57a89539972bd5
ibrahim786in <md.ibrahim786in@gmail.com>
2018-08-31 12:53:04 +0530

    first commit

---
+++ app/src/main/java/com/example/ibrahim/notiicationmsg/MainNotifActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ibrahim.notiicationmsg
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainNotifActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_notif)
+    }
+}
