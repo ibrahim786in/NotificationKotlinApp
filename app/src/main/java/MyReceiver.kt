1538ec6600fb96d708a8fcb2c0e23fbbaf9816c4
ibrahim786in <md.ibrahim786in@gmail.com>
2018-09-04 15:20:49 +0530

    After Single line Custom actions with BroadcastReciever msgs on Notification Panel

--- app/src/main/java/MyReceiver.kt
+++ app/src/main/java/MyReceiver.kt
@@ -11,14 +11,28 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent?) {
 
-        Toast.makeText(context, "Action: " + intent?.action, Toast.LENGTH_LONG).show()
-        val pushNotifBuilder: NotificationCompat.Builder = NotificationCompat.Builder(context, "ChannelId 1")
-                .setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("Simple Notification")
-                .setContentText(intent?.action)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        if (intent!!.action == "Custom Action") {
+            Toast.makeText(context, "Action: " + intent.getStringExtra("CustomText"), Toast.LENGTH_SHORT).show()
 
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(1, pushNotifBuilder.build())
+            val pushNotifBuilder: NotificationCompat.Builder = NotificationCompat.Builder(context, "ChannelId 1")
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setContentTitle("Simple Notification")
+                    .setContentText(intent.getStringExtra("CustomText"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1, pushNotifBuilder.build())
+        } else {
+            Toast.makeText(context, intent.action, Toast.LENGTH_SHORT).show()
+
+            val pushNotifBuilder: NotificationCompat.Builder = NotificationCompat.Builder(context, "ChannelId 1")
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setContentTitle("Simple Notification")
+                    .setContentText(intent.action)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(1, pushNotifBuilder.build())
+        }
     }
 }
--- app/src/main/java/com/example/ibrahim/notiicationmsg/MainNotifActivity.kt
+++ app/src/main/java/com/example/ibrahim/notiicationmsg/MainNotifActivity.kt
@@ -1,14 +1,12 @@
 package com.example.ibrahim.notiicationmsg
 
-import android.app.NotificationManager
 import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
 import android.content.Intent.*
 import android.content.IntentFilter
 import android.os.Bundle
-import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main_notif.*
 
@@ -25,38 +23,36 @@ class MainNotifActivity : AppCompatActivity() {
         filter.addAction(ACTION_AIRPLANE_MODE_CHANGED)
         filter.addAction(ACTION_POWER_DISCONNECTED)
         filter.addAction(ACTION_POWER_CONNECTED)
+        filter.addAction(ACTION_SCREEN_ON)
+        filter.addAction(ACTION_SCREEN_OFF)
+        filter.addAction(ACTION_BATTERY_LOW )
+        filter.addAction("Custom Action")
         registerReceiver(receiver, filter)
 
-        val intent = Intent()
-        intent.action =Intent.ACTION_VIEW
-        sendBroadcast(intent)
 
         pushBtn.setOnClickListener {
             if (pushtxtmsg.text.isEmpty()) Toast.makeText(this, "Please Write Some Content first", Toast.LENGTH_SHORT).show()
             else {
-                val pushNotifBuilder: NotificationCompat.Builder = NotificationCompat.Builder(this, "ChannelId 1")
-                        .setSmallIcon(R.drawable.notification_icon)
-                        .setContentTitle("Simple Notification")
-                        .setContentText(pushtxtmsg.text.toString())
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                val intent = Intent()
+                intent.action = "Custom Action"
+                intent.putExtra("CustomText", pushtxtmsg.text.toString())
+                Log.i("CustTxt", pushtxtmsg.text.toString())
+                sendBroadcast(intent)
+                pushtxtmsg.setText("")
 
-                val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notificationManager.notify(1, pushNotifBuilder.build())
+
             }
         }
-
     }
 
     override fun onResume() {
         registerReceiver(receiver, filter)
         super.onResume()
+        Log.i("Resume", "OnResume")
     }
 
     override fun onPause() {
         super.onPause()
         unregisterReceiver(receiver)
-
     }
-
-
 }
